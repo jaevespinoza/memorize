6e09e14306bdb3830da79b1bd7b9eeb4a6e02812
jespinoza <jespinoza@silta.cl>
2023-09-16 20:22:46 -0300

    Add counter and styles

--- src/components/Game/GameBody.tsx
+++ src/components/Game/GameBody.tsx
@@ -24,11 +24,25 @@ const GameBody = ({ data }: { data: IDuplicatedImage[] }) => {
   );
 
   const foundCards = useSelector((state: RootState) => state.game.foundCards);
+  const successCount = useSelector((state: RootState) => state.game.success);
+  const errorCount = useSelector((state: RootState) => state.game.errors);
 
   return (
     <div className="game-container">
-      <nav className="navbar navbar-dark bg-primary game-container__header">
-        <h3 className="game-container__header__title">Memorize!</h3>
+      <nav className="navbar navbar-dark bg-primary game-container__header fixed-top">
+        <div className="container">
+          <div className="d-flex justify-content-center align-items-center flex-grow-1">
+            <h3 className="game-container__header__title mb-0">Memorize!</h3>
+          </div>
+          <div className="game-container__header__counters text-white">
+            <span className="game-container__header__counters__success">
+              Success: {successCount}
+            </span>
+            <span className="game-container__header__counters__error">
+              Errors: {errorCount}
+            </span>
+          </div>
+        </div>
       </nav>
       <div className="container game-container__body">
         {data.map((item) => (
