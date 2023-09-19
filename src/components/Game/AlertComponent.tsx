82cc94b838154a781f7efeb4bcda20a8834a70aa
jespinoza <jespinoza@silta.cl>
2023-09-18 22:01:18 -0300

    Add testing for alert component

--- src/components/Game/AlertComponent.tsx
+++ src/components/Game/AlertComponent.tsx
@@ -31,32 +31,35 @@ const AlertComponent = () => {
     }
   }, [matchPopup]);
 
+  /**
+   * Function executed when clicking the closed button
+   */
   const handleClose = () => {
     dispatch(setMatchedPopup(false));
+    dispatch(resetCards());
   };
 
   return (
     matchPopup && (
       <div
+        data-testid="popup-message"
         className={`alert ${
           matchSuccess ? "alert-success" : "alert-danger"
         } alert-dismissible fade show position-fixed bottom-0 start-50 translate-middle-x`}
         role="alert"
       >
         <div className="container notification__content">
-          {matchSuccess ? (
-            <>
-              <i className="bi bi-check-circle"></i>{" "}
-              <b className="notification__content__text">Success!</b>
-            </>
-          ) : (
-            <>
-              <i className="bi bi-exclamation-triangle"></i>
-              <b className="notification__content__text"> Error!</b>
-            </>
-          )}
+          <i
+            className={`bi ${
+              matchSuccess ? "bi-check-circle" : "bi-exclamation-triangle"
+            }`}
+          ></i>{" "}
+          <b className="notification__content__text" data-testid="message">
+            {matchSuccess ? "Success!" : "Error!"}
+          </b>
           <button
             type="button"
+            data-testid="close-button"
             className="btn-close"
             onClick={handleClose}
             aria-label="Close"
---
+++ src/components/Game/Game.test.tsx
@@ -0,0 +1,73 @@
+import { act, render, screen } from "@testing-library/react";
+
+import AlertComponent from "./AlertComponent";
+import { beforeEach, describe, expect, test } from "vitest";
+import { configureStore } from "@reduxjs/toolkit";
+import { ToolkitStore } from "@reduxjs/toolkit/dist/configureStore";
+import GameReducer, {
+  IGameState,
+  setMatchedCard,
+  setMatchedPopup,
+} from "../../actions/GameReducer";
+import { Provider } from "react-redux";
+
+describe("Alert Component", () => {
+  let store: ToolkitStore<{ game: IGameState }>;
+
+  beforeEach(() => {
+    store = configureStore({ reducer: { game: GameReducer } });
+  });
+
+  test("renders properly", () => {
+    const alert = render(
+      <Provider store={store}>
+        <AlertComponent />
+      </Provider>
+    );
+
+    expect(alert).toMatchSnapshot();
+  });
+
+  test("has Error message", () => {
+    store.dispatch(setMatchedPopup(true));
+
+    render(
+      <Provider store={store}>
+        <AlertComponent />
+      </Provider>
+    );
+
+    expect(screen.getByTestId("message").textContent).toBe("Error!");
+  });
+
+  test("has Success message", () => {
+    store.dispatch(setMatchedPopup(true));
+    store.dispatch(setMatchedCard(true));
+
+    render(
+      <Provider store={store}>
+        <AlertComponent />
+      </Provider>
+    );
+
+    expect(screen.getByTestId("message").textContent).toBe("Success!");
+  });
+
+  test("closes Message", () => {
+    store.dispatch(setMatchedPopup(true));
+    store.dispatch(setMatchedCard(true));
+
+    render(
+      <Provider store={store}>
+        <AlertComponent />
+      </Provider>
+    );
+
+    act(() => {
+      screen.getByTestId("close-button").click();
+    });
+
+    expect(store.getState().game.matchPopup).toBe(false);
+    expect(screen.queryByTestId("popup-message")).toBe(null);
+  });
+});
